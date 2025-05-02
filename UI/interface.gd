ab9a5bb8c3add6cf464c193552b65d559bc6fccc
LeoTengAMao <LeoTeng995@gmail.com>
2025-05-02 22:16:14 +0800

    Button 測試
    skill 1 skill 2 backpack按鈕測試

---
+++ UI/interface.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_skill_2_pressed() -> void:
+	print("skill2")
+
+
+func _on_skill_1_pressed() -> void:
+	print("skill1")
+
+
+func _on_back_pack_pressed() -> void:
+	print("Backpack")
