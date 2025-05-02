934d8be9aca98e0478d96998b8af023518c89420
monjo123 <wenocean123@gmail.com>
2025-05-03 00:26:07 +0800

    RestArea

---
+++ RestArea/scripts/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+@export var move_speed : float = 100
+
+func _process(delta: float) -> void:
+	if get_tree().current_scene.in_store and Input.is_action_just_pressed("interaction"):
+		print("buy sword")
+
+func _physics_process(delta: float) -> void:
+		# print(Input.get_vector("left", "right", "up", "down"))
+	velocity = Input.get_vector("left", "right", "up", "down") * move_speed
+	
+	move_and_slide()
