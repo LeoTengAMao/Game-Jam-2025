216d39e4019dc8ac5c2b340bc5311ca76d83a0e5
MysteryStarfish <paulhsu1016@gmail.com>
2025-05-04 01:53:11 +0800

    fix: maybe missing files

--- script/Entity/Entity.cs
+++ script/Entity/Entity.cs
@@ -2,30 +2,30 @@ namespace 新遊戲專案.script;
 
 public abstract class Entity
 {
-    protected int MaxHp { get; private set; }
-    protected int Hp { get; private set; }
-    protected int Atk { get; private set; }
-    protected int Def { get; private set; }
-    protected int Spd { get; private set; }
+	protected int MaxHp { get; private set; }
+	protected int Hp { get; private set; }
+	protected int Atk { get; private set; }
+	protected int Def { get; private set; }
+	protected int Spd { get; private set; }
 
-    protected Entity(int hp, int atk, int def, int spd)
-    {
-        MaxHp = hp;
-        Hp = hp;
-        Atk = atk;
-        Def = def;
-        Spd = spd;
-    }
+	protected Entity(int hp, int atk, int def, int spd)
+	{
+		MaxHp = hp;
+		Hp = hp;
+		Atk = atk;
+		Def = def;
+		Spd = spd;
+	}
 
-    public virtual void TakeDamage(int damage)
-    {
-        int loseHp = damage - Def;
-        if (loseHp < 1) loseHp = 1;
-        Hp -= loseHp;
-    }
+	public virtual void TakeDamage(int damage)
+	{
+		int loseHp = damage - Def;
+		if (loseHp < 1) loseHp = 1;
+		Hp -= loseHp;
+	}
 
-    public bool IsDead()
-    {
-        return Hp <= 0;
-    }
+	public bool IsDead()
+	{
+		return Hp <= 0;
+	}
 }
---
+++ script/Skill/Skill.cs
@@ -0,0 +1,13 @@
+namespace 新遊戲專案.script;
+
+public class Skill : AbstractSkill
+{
+    public Skill(int actionPoint) : base(actionPoint)
+    {
+    }
+
+    public override void Execute(Entity target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
