5ccfeb84df3b072952010c6b5654f89d93ec93be
MysteryStarfish <paulhsu1016@gmail.com>
2025-05-03 21:27:50 +0800

    feat: impl simple Turn-Based system
    add player turn and enemy turn in game_combatTest2 scene

--- script/EnemyGenerateHandler.cs
+++ script/EnemyGenerateHandler.cs
@@ -1,30 +1,32 @@
 using System.Collections.Generic;
 using Godot;
+using 新遊戲專案.scenes;
 
 namespace 新遊戲專案.script;
 
 public partial class EnemyGenerateHandler : Node
 {
-    [Export] private int _enemyNumbers;
-    [Export] private Godot.Collections.Array<PackedScene> _enemies = new();
-    [Export] private Godot.Collections.Array<Node2D> _positions = new();
-    private Slime[] targets = new Slime[1];
-    public override void _Ready()
-    {
-        Init();
-    }
-    private void Init()
-    {
-        var randomIndex = GD.Randi() % _enemies.Count;
-        var enemyScene = _enemies[(int)randomIndex];
-        var enemy = (Slime)enemyScene.Instantiate();
-        targets[0] = enemy;
-        AddChild(enemy);
-        enemy.GetNode<Node2D>(null).Position = new Vector2(_positions[(int)randomIndex].Position.X, _positions[(int)randomIndex].Position.Y);
-    }
+	private int _enemyNumbers;
+	[Export] private Godot.Collections.Array<PackedScene> _enemies = new();
+	[Export] private Godot.Collections.Array<Node2D> _positions = new();
+	private BasicEnemy[] _targets = new BasicEnemy[1];
+	public override void _Ready()
+	{
+		Init();
+	}
+	private void Init()
+	{
+		_enemyNumbers = _positions.Count;
+		var randomIndex = GD.Randi() % _enemies.Count;
+		var enemyScene = _enemies[(int)randomIndex];
+		var enemy = (BasicEnemy)enemyScene.Instantiate();
+		_targets[0] = enemy;
+		AddChild(enemy);
+		enemy.GetNode<Node2D>(null).Position = new Vector2(_positions[(int)randomIndex].Position.X, _positions[(int)randomIndex].Position.Y);
+	}
 
-    public Enemy GetEnemyByIdx(int idx)
-    {
-        return targets[idx].Enemy;
-    }
+	public Enemy GetEnemyByIdx(int idx)
+	{
+		return _targets[idx].Enemy;
+	}
 }
--- script/Entity/Enemy.cs
+++ script/Entity/Enemy.cs
@@ -2,7 +2,12 @@ using 新遊戲專案.script;
 
 public partial class Enemy : Entity
 {
+	public int CurrentHp => Hp ;
+	public int CurrentAtk => Atk ;
+	public int CurrentDef => Def ;
+	public int CurrentSpd => Spd ;
 	public Enemy(int hp, int atk, int def, int spd) : base(hp, atk, def, spd)
 	{
+		
 	}
 }
