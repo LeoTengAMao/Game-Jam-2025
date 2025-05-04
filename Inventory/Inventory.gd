b0c17634c668dca1bdf5174524ae8dc658390e7e
LeoTengAMao <LeoTeng995@gmail.com>
2025-05-04 10:45:20 +0800

    add Equiment

--- Inventory/Inventory.gd
+++ Inventory/Inventory.gd
@@ -9,6 +9,13 @@ func AddItems(new_stack: InvItem) -> bool:
 			items[i] = new_stack
 			return true
 	return false  # 沒空格
+	
+func AddEquiment(new_stack: InvItem) -> bool:
+	for i in range(10,items.size()):
+		if items[i] == null:
+			items[i] = new_stack
+			return true
+	return false  # 沒空格
 
 func RemoveItems(target_stack: InvItem) -> void:
 	if items.has(target_stack):
--- scenes/RestArea/scripts/player.gd
+++ scenes/RestArea/scripts/player.gd
@@ -5,12 +5,25 @@ extends CharacterBody2D
 
 
 func _process(delta: float) -> void:
+	
 	if Input.is_action_just_pressed("interaction"):
 		if GlobalRest.in_shop:
 			get_tree().change_scene_to_file("res://scenes/RestArea/scenes/store.tscn")
+		if GlobalRest.in_stair:
+			get_tree().change_scene_to_file("res://scenes/RestArea/scenes/game.tscn")
 		
 
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * move_speed
+	if velocity.length() > 0:
+		# 設定移動動畫
+		$AnimatedSprite2D.play("run")  
+		# 根據方向翻轉角色或改變動畫方向
+		if abs(velocity.x) > abs(velocity.y):
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		# 可加入判斷上下方向的動畫切換
+	else:
+		# 停下時播放靜止動畫
+		$AnimatedSprite2D.play("idle") 
 	move_and_slide()
 	
