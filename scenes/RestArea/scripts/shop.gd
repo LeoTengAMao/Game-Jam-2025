a0888a405badaabb02b7de02fbf75f1cd99efa38
LeoTengAMao <LeoTeng995@gmail.com>
2025-05-04 13:14:04 +0800

    HealthBar update and GUI update

--- scenes/RestArea/scripts/shop.gd
+++ scenes/RestArea/scripts/shop.gd
@@ -1,15 +1,18 @@
 extends Area2D
 
 @export var shop_intro : Label
+
 	
 func _on_body_entered(body: Node2D) -> void:
-	print("enter next level")
-	GlobalRest.in_shop = true
-	shop_intro.visible = true
+	if body.name == "player":
+		print("enter next level")
+		GlobalRest.in_shop = true
+		shop_intro.visible = true
 
 
 func _on_body_exited(body: Node2D) -> void:
-	print("exit shop")
-	GlobalRest.in_shop = false
-	shop_intro.visible = false
+	if body.name == "player":
+		print("exit shop")
+		GlobalRest.in_shop = false
+		shop_intro.visible = false
 	
