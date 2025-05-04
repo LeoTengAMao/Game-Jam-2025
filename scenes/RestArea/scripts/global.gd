1bcf0b279e5c342466dd9208303df58eb3c17f12
monjo123 <wenocean123@gmail.com>
2025-05-04 13:23:13 +0800

    updata RestArea

--- scenes/RestArea/scripts/global.gd
+++ scenes/RestArea/scripts/global.gd
@@ -3,5 +3,6 @@ extends Node
 var player_pos : Vector2
 
 var in_shop : bool 
+var in_shop1 : bool 
 
 var in_stair : bool 
--- scenes/RestArea/scripts/shop1.gd
+++ scenes/RestArea/scripts/shop1.gd
@@ -5,13 +5,13 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("enter next level")
-		GlobalRest.in_shop = true
+		GlobalRest.in_shop1 = true
 		shop_intro.visible = true
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print("exit shop")
-		GlobalRest.in_shop = false
+		GlobalRest.in_shop1 = false
 		shop_intro.visible = false
-		
+	
