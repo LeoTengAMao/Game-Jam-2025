c58b5c090a012fd5cf9c7028468435b6a445f4f3
LeoTengAMao <LeoTeng995@gmail.com>
2025-05-04 13:15:17 +0800

    Hotfix Rest

--- scenes/RestArea/scripts/Stair.gd
+++ scenes/RestArea/scripts/Stair.gd
@@ -3,13 +3,13 @@ extends Area2D
 @export var Stair_intro : Label
 	
 func _on_body_entered(body: Node2D) -> void:
-	
-	GlobalRest.in_stair = true
-	Stair_intro.visible = true
+	if body.name == "player":	
+		GlobalRest.in_stair = true
+		Stair_intro.visible = true
 
 
 func _on_body_exited(body: Node2D) -> void:
-
-	GlobalRest.in_stair = false
-	Stair_intro.visible = false
-	
+	if body.name == "player":
+		GlobalRest.in_stair = false
+		Stair_intro.visible = false
+		
