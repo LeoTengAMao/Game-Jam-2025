a160929fa3d5ebc58a9ae0551fd6c71b9f622992
LeoTengAMao <LeoTeng995@gmail.com>
2025-05-04 12:41:04 +0800

    GUI update and RestArea fixed

--- scenes/RestArea/scripts/Stair.gd
+++ scenes/RestArea/scripts/Stair.gd
@@ -1,14 +1,15 @@
 extends Area2D
 
-@export var shop_intro : Label
+@export var Stair_intro : Label
 	
 func _on_body_entered(body: Node2D) -> void:
-	Global.Floor +=1
-	shop_intro.visible = true
+	
+	GlobalRest.in_stair = true
+	Stair_intro.visible = true
 
 
 func _on_body_exited(body: Node2D) -> void:
-	
-	GlobalRest.in_shop = false
-	shop_intro.visible = false
+
+	GlobalRest.in_stair = false
+	Stair_intro.visible = false
 	
